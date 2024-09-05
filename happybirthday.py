2d477de78d8c33d6db1b6b0ae4a92e966878a15e
ak2986 <ak2986@rit.edu>
2024-09-05 09:53:28 +0400

    Add files via upload

---
+++ happybirthday.py
@@ -0,0 +1,10 @@
+
+name=input("please enter your name")
+birthday=input("please enter your birthday")
+birthdate=input("please enter your birthmonth")
+birthmonth=input("please enter your birthmonth")
+birthyear=input("please enter your birth year")
+print(name, 'your birthday is on',birthmonth,',',birthday,',',birthyear,'!')
+
+
+
